65e63824479b4b6db9889f4681355293d3f4bbfa
Beskhue <thomas@kepow.org>
2017-02-21 00:08:15 +0100

    Use clize to create an interface for the program entry-point.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
+from clize import run
 import pipeline
+import pprint
 
-pl = pipeline.Pipeline()
+def example():
+    """
+    Run the pipeline example (tests if the pipeline runs succesfully, should output ).
+    """
 
-a = pl.train_and_validation_mini_batch_generator_generator()
-train_mini_batches = a['train']
+    pl = pipeline.Pipeline()
 
-for x, y, meta in train_mini_batches:
-    print(len(x), len(y))
+    a = pl.train_and_validation_mini_batch_generator_generator()
+    train_mini_batches = a['train']
+
+    x, y, meta = next(train_mini_batches)
+    print("Number of cases in first batch: %s" % len(x))
+    print("First image shape and label: %s - %s" % (str(x[0]().shape), y[0]))
+    print("First image meta information:")
+    pprint.pprint(meta[0])
+
+if __name__ == "__main__":
+    run(example)
