edce989c7b21d656535e1a2c845932b6a122d9a6
Beskhue <thomas@kepow.org>
2017-03-27 22:39:45 +0200

    Improve settings documentation.

--- src/settings.py
+++ src/settings.py
@@ -5,16 +5,26 @@ Module containing the project settings.
 import os
 from time import strftime
 
+# Directory settings
+
 ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 OUTPUT_DIR = os.path.join(ROOT_DIR, "output")
 
+## Input directories
+
 TRAIN_DIR = os.path.join(DATA_DIR, "train")
 TRAIN_BOUNDING_BOXES_DIR = os.path.join(DATA_DIR, "bounding_boxes")
 TEST_DIR = os.path.join(DATA_DIR, "test")
 
+## Output directories
+
 CONVERTED_BOUNDING_BOX_OUTPUT_DIR = os.path.join(OUTPUT_DIR, "bounding_boxes", strftime("%Y%m%dT%H%M%S"))
+
+# Problem-specific settings
+
+## Classes
 
 CLASS_NAME_TO_INDEX_MAPPING = {
     "ALB": 0,
