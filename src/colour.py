b71a477ad98b1a55ab7e7e1c6997dcb927c21685
Jordi <jordi.riemens@student.ru.nl>
2017-03-29 14:57:05 +0200

    Implement segmentation for candidate selection

--- src/colour.py
+++ src/colour.py
@@ -12,6 +12,11 @@ def intensity(vc):
     return np.sqrt(np.power(acc(vc,0),2) + np.power(acc(vc,1),2) + np.power(acc(vc,2),2))
     
 def colourdist(frm, to):
+    """
+    Computes the distance from colour frm to another colour to.
+    Exact formula is sqrt(norm(from)^2 - dot(from,to)/norm(to)^2)
+    which is equivalent to norm(from)*sqrt(1 - cos angle(from,to))
+    """
     If = acc(frm, 0)**2 + acc(frm, 1)**2 + acc(frm, 2)**2 
     It = acc(to, 0)**2 + acc(to, 1)**2 + acc(to, 2)**2 
     dot = acc(frm, 0)*acc(to, 0) + acc(frm, 1)*acc(to, 1) + acc(frm, 2)*acc(to, 2)
@@ -21,6 +26,11 @@ def brightnessmatch(I, clmeta, alpha=0.5, beta=1.25):
     return I >= alpha*clmeta[2] and I <= min(beta * clmeta[2], clmeta[1]/alpha)
 
 def colourchange(img):
+    """
+    Compute the change in colour around each pixel, by
+    computing the colour distance between the pixels to the 
+    left and right, as well as the up and down.
+    """
     imgl = skimage.transform.warp(img, skimage.transform.AffineTransform(translation=(1, 0)).inverse, mode='edge')
     imgr = skimage.transform.warp(img, skimage.transform.AffineTransform(translation=(-1,0)).inverse, mode='edge')
     imgu = skimage.transform.warp(img, skimage.transform.AffineTransform(translation=(0, 1)).inverse, mode='edge')
@@ -30,6 +40,7 @@ def colourchange(img):
     diffimg = np.sqrt(difflr**2 + diffud**2)
     return (255*preprocessing.clamp(diffimg / np.percentile(diffimg, 99))).astype(np.uint8)
 
+NIGHT_VISION_THRESHOLD = 1.3 # threshold on mean(G)/mean(R+G+B)
 def is_night_vision(img):
     """
     Returns true when an image is taken with a night vision camera,
@@ -38,5 +49,3 @@ def is_night_vision(img):
     :param img: The image to test.
     """
     return np.mean(img[:,:,1]) / np.mean(img) > NIGHT_VISION_THRESHOLD
-
-NIGHT_VISION_THRESHOLD = 1.3 # threshold on mean(G)/mean(R+G+B)
---
+++ src/segmentation.py
@@ -0,0 +1,191 @@
+import colour
+import matplotlib.cm
+import matplotlib.colors
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import pipeline
+import preprocessing
+import random
+import skimage.color
+import skimage.exposure
+import skimage.filters
+import skimage.future.graph
+import skimage.measure
+import skimage.segmentation
+import skimage.util
+
+
+# this library function did not work together with the below functions so we change one line with a monkey-patch..
+def _merge_nodes(self, src, dst, weight_func=skimage.future.graph.rag.min_weight, in_place=True,
+                extra_arguments=[], extra_keywords={}):
+    src_nbrs = set(self.neighbors(src))
+    dst_nbrs = set(self.neighbors(dst))
+    neighbors = (src_nbrs | dst_nbrs) - set([src, dst])
+
+    if in_place:
+        new = dst
+    else:
+        new = self.next_id()
+        self.add_node(new)
+
+    for neighbor in neighbors:
+        w = weight_func(self, src, new, neighbor, *extra_arguments,
+                        **extra_keywords)
+        self.add_edge(neighbor, new, attr_dict=w)
+
+    self.node[new]['labels'] = (self.node[src]['labels'] +
+                                self.node[dst]['labels'])
+    self.remove_node(src)
+
+    if not in_place:
+        self.remove_node(dst)
+
+    return new
+skimage.future.graph.RAG.merge_nodes = _merge_nodes
+
+# two functions below adapted from scikit-image example code
+def _merge_boundary(graph, src, dst):
+    pass
+
+def _weight_boundary(graph, src, dst, n):
+    default = {'weight': 0.0, 'count': 0}
+    count_src = graph[src].get(n, default)['count']
+    count_dst = graph[dst].get(n, default)['count']
+    count = count_src + count_dst
+    weight_src = graph[src].get(n, default)['weight']
+    weight_dst = graph[dst].get(n, default)['weight']
+    return { 'count': count, 'weight': (count_src * weight_src + count_dst * weight_dst)/count }
+
+def colour_segmentation(img, num_segments=1000, round_schedule = [0.02, 0.04, 0.06, 0.08], colour_median_prop=0, max_clust_size=0.05, min_clust_size=0.002):
+    """
+    Segment an image into clusters based on the colours in the image.
+    Description of the algorithm can be found in comments in the code.
+    Default parameters were tuned for optimal recall, so many false positives will be found.
+    :param img: Image to segment
+    :param num_segments: Number of segments to segment the image into, prior to merging
+    :param round_schedule: Distance threshold for merging two clusters, per round
+    :param colour_median_prop: Replace all pixels in the clusters whose areas exceed colour_median_prop*image_size by their median colour
+    :param max_clust_size: Discard all clusters with areas larger than max_clust_size*image_size
+    :param min_clust_size: Discard all clusters with areas smaller than min_clust_size*image_size
+    """
+    origimg = img
+    
+    # Initial segmentation
+    regions = skimage.segmentation.slic(img, n_segments=num_segments)
+
+    for round_thr in round_schedule:
+        # Compute colour change of each pixel
+        edges = skimage.util.dtype.img_as_float(colour.colourchange(img))
+        
+        # Merge clusters hierarchically based on above distance
+        rag = skimage.future.graph.rag_boundary(regions, edges)
+        regions = skimage.future.graph.merge_hierarchical(regions, rag, thresh=round_thr, rag_copy=False, in_place_merge=True, merge_func=_merge_boundary, weight_func=_weight_boundary)
+
+        # Replace all pixels in (some?) clusters with their median colour
+        clust_sizes = skimage.exposure.histogram(regions)[0]
+        clust_sizes = clust_sizes / float(sum(clust_sizes))
+        medianclusters = np.where(clust_sizes > colour_median_prop)[0]
+
+        img = origimg.copy()
+        for mediancluster in medianclusters:
+            img[regions == mediancluster] = np.median(img[regions == mediancluster], axis=0)
+        
+        if len(clust_sizes) == 1:
+            break
+    
+    # Filter out too small and too large clusters
+    num_clusters = 0
+    for clust in range(len(clust_sizes)):
+        if clust_sizes[clust] > max_clust_prop or clust_sizes[clust] < min_clust_prop: # background or noise resp.
+            regions[regions == clust] = 0
+        else:
+            num_clusters += 1
+            regions[regions == clust] = num_clusters
+    
+    # Extract centroids
+    centroids = [clust.centroid for clust in skimage.measure.regionprops(regions)]
+    
+    return (regions, centroids)
+
+
+# Test script
+if __name__ == '__main__':
+    # Parameters
+    bbox_size = 300
+    overlap_ratio = 0.65
+    
+    # Load images
+    dl = pipeline.DataLoader()
+    train_imgs = dl.get_train_images_and_classes(lambda img, y, meta: img)
+
+    train_x = train_imgs['x']
+    train_y = train_imgs['y']
+    train_meta = train_imgs['meta']
+    
+    random.seed(42)
+    train_idxs = list(range(len(train_x)))
+    random.shuffle(train_idxs)
+    
+    # Prepare measurements
+    tp_boxes = 0
+    num_boxes = 0
+    tp_fish = 0
+    num_fish = 0
+    num_impossible = 0
+    
+    # Prepare histogram matching template
+    template = preprocessing.build_template(train_x, train_meta)
+    
+    for idx_idx in range(len(train_x)):
+        idx = train_idxs[idx_idx]
+        
+        # Load image
+        img = train_x[idx]()
+        imgclass = train_y[idx]
+        if 'bounding_boxes' in train_meta[idx]:
+            imgboxes = train_meta[idx]['bounding_boxes']
+        else:
+            imgboxes = []
+        
+        # Use histogram matching for night vision images
+        nvg = False
+        if colour.is_night_vision(img): # night vision
+            nvg = True
+            img = preprocessing.hist_match(img, template)
+        
+        # Perform actual segmentation
+        regions, centroids = colour_segmentation(img)
+        
+        # See how well the centroids match
+        lower = lambda centroid, dim: max(centroid[dim] - bbox_size/2.0, 0)
+        upper = lambda centroid, dim: min(centroid[dim] + bbox_size/2.0-1, img.shape[dim])
+        intersection = lambda bbox, centroid: max(0, min(upper(centroid, 1), bbox['x']+bbox['width']) - max(lower(centroid, 1), bbox['x'])) * max(0, min(upper(centroid, 0), bbox['y']+bbox['height']) - max(lower(centroid, 0), bbox['y']))
+        matches_centroid = lambda bbox, centroid: intersection(bbox, centroid) / float(bbox['width']*bbox['height']) >= overlap_ratio
+        
+        num_matching_boxes = sum(any(matches_centroid(bbox, centroid) for bbox in imgboxes) for centroid in centroids)
+        num_found_fish = sum(any(matches_centroid(bbox, centroid) for centroid in centroids) for bbox in imgboxes)
+        num_impossible_here = sum(max(bbox['width'], bbox['height'])*overlap_ratio-1 >= bbox_size for bbox in imgboxes)
+        
+        # Record this information
+        tp_boxes += num_matching_boxes
+        num_boxes += len(centroids)
+        tp_fish += num_found_fish
+        num_fish += len(imgboxes) - num_impossible_here
+        num_impossible += num_impossible_here
+        
+        # Output performance for this image
+        print('Image %d (found %d/%d%s, %d FPs%s)' % (idx, num_found_fish, len(imgboxes)-num_impossible_here, (", %d impossible" % num_impossible_here) if num_impossible_here > 0 else "", len(centroids)-num_matching_boxes, "; NVG" if nvg else ""))
+        
+        # Summarise performance up till now
+        if idx_idx%50 == 49:
+            box_precision = 100*tp_boxes / float(num_boxes)
+            fish_recall = 100*tp_fish / float(num_fish)
+            
+            print("Box precision after %d images: %g%% (%d/%d)\nFish recall after %d images: %g%% (%d/%d%s)\n" % (idx_idx+1, box_precision, tp_boxes, num_boxes, idx_idx+1, fish_recall, tp_fish, num_fish, (", %d impossible" % num_impossible) if num_impossible > 0 else ""))
+    
+    # Summarise total performance
+    box_precision = 100*tp_boxes / float(num_boxes)
+    fish_recall = 100*tp_fish / float(num_fish)
+    
+    print("\nTotal box precision: %g%% (%d/%d)\nTotal fish recall: %g%% (%d/%d%s)\n" % (box_precision, tp_boxes, num_boxes, fish_recall, tp_fish, num_fish, (", %d impossible" % num_impossible) if num_impossible > 0 else ""))
