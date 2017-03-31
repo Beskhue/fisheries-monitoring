9307a5142fd66c7c0f515c8b7118c139bd2f83ec
Beskhue <thomas@kepow.org>
2017-03-31 02:07:19 +0200

    Add an augmentor class using Keras' ImageDataGenerator

---
+++ src/augmentor.py
@@ -0,0 +1,16 @@
+import keras.preprocessing.image
+import keras.backend as K
+
+
+class Augmentor(ImageDataGenerator):
+    """
+    Use Keras' Image Data Generator to augment an image
+    """
+
+    def __init__(self, augmentor):
+        self.augmentor = augmentor
+
+    def augment(self, x):
+        x = self.augmentor.random_transform(x.astype(K.floatx()))
+        x = self.augmentor.standardize(x)
+        return x
