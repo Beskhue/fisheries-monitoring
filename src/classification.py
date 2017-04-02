886e77d6c7e9cc339d7636746b31ef04569aa343
Jordi <jordi.riemens@student.ru.nl>
2017-04-02 19:45:39 +0200

    Fix miscellaneous bugs and complete classification.py

--- src/classification.py
+++ src/classification.py
@@ -1,3 +1,4 @@
+from clize import run
 import os
 import functools
 import json
@@ -42,15 +43,18 @@ class FullClassification:
         Stage 3
         """
 
-        mini_batch_size = 32
+        mini_batch_size = 32 # decrease in case of resource exhausted errors
         
         import keras
+        import metrics
+        import shutil
+        from time import strftime
         
         ppl = pipeline.Pipeline(data_type = "candidates_cropped", dataset = self.dataset)
         self.prepare_directories()
 
         # Load fish-or-no-fish classification model
-        model = keras.models.load_model(os.path.join(settings.WEIGHTS_DIR, settings.FISH_OR_NO_FISH_CLASSIFICATION_NETWORK_WEIGHT_NAME))
+        model = keras.models.load_model(os.path.join(settings.WEIGHTS_DIR, settings.FISH_OR_NO_FISH_CLASSIFICATION_NETWORK_WEIGHT_NAME), custom_objects={'precision': metrics.precision, 'recall': metrics.recall})
 
         data_generator = ppl.data_generator_builder(
             functools.partial(ppl.mini_batch_generator, mini_batch_size = mini_batch_size))
@@ -66,7 +70,7 @@ class FullClassification:
             predictions = model.predict(x, batch_size = mini_batch_size)
 
             for m, pred in zip(meta, list(predictions)):
-                predicted[m['filename']] = bool(round(pred[0]))
+                predicted[m['filename']] = pred[0]
             
             n_batches += 1
             if n_batches % batch_print_interval == 0:
@@ -76,8 +80,12 @@ class FullClassification:
         if not os.path.exists(self.fish_or_no_fish_classification_dir):
             os.makedirs(self.fish_or_no_fish_classification_dir)
         
-        with open(os.path.join(self.fish_or_no_fish_classification_dir, "classification.json"), 'w') as outfile:
+        outpath = os.path.join(self.fish_or_no_fish_classification_dir, "classification.json")
+        outpath2 = os.path.join(self.fish_or_no_fish_classification_dir, "classification-%s.json" % strftime("%Y%m%dT%H%M%S"))
+        with open(outpath, 'w') as outfile:
             json.dump(predicted, outfile)
+        
+        shutil.copyfile(outpath, outpath2)
 
     def classify_fish_type(self):
         """
@@ -85,40 +93,55 @@ class FullClassification:
         """
 
         import keras
+        import metrics
+        import shutil
+        from time import strftime
+        
+        threshold = 0.5
 
         ppl = pipeline.Pipeline(data_type = "candidates_cropped", dataset = self.dataset)
         self.prepare_directories()
 
         # Load fish type classification model
-        model = keras.models.load_model(os.path.join(settings.WEIGHTS_DIR, settings.FISH_TYPE_CLASSIFICATION_NETWORK_WEIGHT_NAME))
+        model = keras.models.load_model(os.path.join(settings.WEIGHTS_DIR, settings.FISH_TYPE_CLASSIFICATION_NETWORK_WEIGHT_NAME), custom_objects={'precision': metrics.precision, 'recall': metrics.recall})
 
         # Load fish-or-no-fish classifications
-        with open(os.path.join(self.fish_or_no_fish_classification_dir, "classification.json"), 'r') as infile:
+        inpath = os.path.join(self.fish_or_no_fish_classification_dir, "classification.json")
+        with open(inpath, 'r') as infile:
             fish_or_no_fish = json.load(infile)
 
         data = ppl.get_data()
 
         fish_type_classification = {}
+        n_imgs = 0
 
         # For each single crop
         for x, meta in zip(data['x'], data['meta']):
-            x = np.array(x)
+            #x = np.array(x)
 
-            if fish_or_no_fish[meta['filename']]:
+            if fish_or_no_fish[meta['filename']] > 0.5:
 
                 img = x()
                 img = np.array([img])
 
                 predictions = model.predict(img, batch_size = 1)
                 
-                fish_type_classification[meta['filename']] = predictions.tolist()
+                fish_type_classification[meta['filename']] = predictions.tolist()[0]
+            
+            n_imgs += 1
+            if n_imgs % 100 == 0:
+                print('%d candidates processed' % n_imgs)
     
         # Save classifications
-        if not os.path.exists(self.fish_or_no_fish_classification_dir):
+        if not os.path.exists(self.fish_type_classification_dir):
             os.makedirs(self.fish_type_classification_dir)
         
-        with open(os.path.join(self.fish_type_classification_dir, "classification.json"), 'w') as outfile:
+        outpath = os.path.join(self.fish_type_classification_dir, "classification.json")
+        outpath2 = os.path.join(self.fish_type_classification_dir, "classification-%s.json" % strftime("%Y%m%dT%H%M%S"))
+        with open(outpath, 'w') as outfile:
             json.dump(fish_type_classification, outfile)
+        
+        shutil.copyfile(outpath, outpath2)
 
     def classify_image(self):
         """
@@ -126,10 +149,15 @@ class FullClassification:
         """
         
         import csv
+        import math
+        import shutil
+        from time import strftime
+        
         self.prepare_directories()
 
         # Load fish type classifications
-        with open(os.path.join(self.fish_type_classification_dir, "classification.json"), 'r') as infile:
+        inpath = os.path.join(self.fish_type_classification_dir, "classification.json")
+        with open(inpath, 'r') as infile:
             fish_type_classification = json.load(infile)
 
         ppl = pipeline.Pipeline(data_type = "candidates_cropped", dataset = self.dataset)
@@ -143,34 +171,64 @@ class FullClassification:
 
             if name in fish_type_classification:
                 # There is a classification for this crop
-
                 original_img = meta['original_image']
 
                 if original_img not in cand_classifications:
-                    cand_classifications = []
+                    cand_classifications[original_img] = []
 
-                cand_classifications.append(fish_type_classification[name])
-
+                cand_classifications[original_img].append(fish_type_classification[name])
+        
+        outpath2 = os.path.join(self.fish_type_classification_dir, "aggregatedclassification-%s.json" % strftime("%Y%m%dT%H%M%S"))
+        with open(outpath2, 'w') as outfile:
+            json.dump(cand_classifications, outfile)
+        
         # Perform something to turn list of classifications for an image to scores for all classes
-        img_classification = {}
+        img_classifications = {}
 
         pipeline_original = pipeline.Pipeline(data_type = "original", dataset = self.dataset)
         original_images = pipeline_original.get_data()
         for meta in original_images['meta']:
             name = meta['filename']
 
-            # TODO Use list of 7-class classification scores to generate one single 8-class classification score
-            if name not in img_classifications:
-                img_classification['name'] = settings.CLASS_NAME_TO_INDEX_MAPPING["NoF"]
+            # Use list of 7-class classification scores to generate one single 8-class classification score (how to deal with NoF?)
+            if name not in cand_classifications: # only happens if fish-or-no-fish is too strict
+                print('Image has zero detected fish candidates: ' + name)
+                img_classifications[name] = [0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.993]
             else:
-                classifications = img_classifications[name]
+                img_classification = [0,0,0,0,0,0,0,0]
+                for cand_classification in cand_classifications[name]:
+                    # add up scores, score(NoF) = 1 - max(score for any fish type)
+                    print(cand_classification)
+                    img_classification = [a+b for a,b in zip(img_classification, cand_classification + [1-max(cand_classification)])]
+                
+                # softmax
+                beta = 1
+                img_classification = [math.exp(beta*(score - max(img_classification))) for score in img_classification]
+                img_classification = [score/sum(score) for score in img_classification]
+                img_classifications[name] = img_classification
+                
                 
         # Output in kaggle format
-        with open(os.path.join(self.fish_type_classification_dir, "submission.json"), 'w', newline='') as subm_file:
+        class_order = [0, 1, 2, 3, 7, 6, 4, 5]
+        outpath = os.path.join(self.fish_type_classification_dir, "submission.json")
+        outpath2 = os.path.join(self.fish_type_classification_dir, "submission-%s.json" % strftime("%Y%m%dT%H%M%S"))
+        with open(outpath, 'w', newline='') as subm_file:
             subm_writer = csv.writer(subm_file)
-            subm_writer.writerow(['image','ALB','BET','DOL','LAG','NoF','OTHER','SHARK','YFT'])
+            subm_writer.writerow(['image', 'ALB','BET','DOL','LAG','NoF','OTHER','SHARK','YFT'])
             
             for meta in original_images['meta']:
                 name = meta['filename']
-                # TODO hook below line up to img_classification
-                subm_writer.writerow([name + '.jpg', 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125])
+                img_classification = img_classifications[name]
+                img_classification = [img_classification[i] for i in class_order] # permute into Kaggle order
+                subm_writer.writerow([name + '.jpg'] + img_classification)
+        
+        shutil.copyfile(outpath, outpath2)
+
+
+if __name__ == "__main__":
+    fc = FullClassification()
+    run(fc.propose_candidates,
+        fc.crop_candidates,
+        fc.classify_fish_or_no_fish,
+        fc.classify_fish_type,
+        fc.classify_image)
