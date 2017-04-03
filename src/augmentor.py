8baffb402bac93c749ee63b30531a0ff9701c62f
hipoglucido <gcvictorgc@gmail.com>
2017-04-03 19:11:01 +0200

    Had to change code because of blur.

--- src/augmentor.py
+++ src/augmentor.py
@@ -9,11 +9,12 @@ class Augmentor(keras.preprocessing.image.ImageDataGenerator):
     Use Keras' Image Data Generator to augment an image
     """
 
-    def __init__(self, imageDataGenerator):
+    def __init__(self, imageDataGenerator, augmentation_mode):
         self.imageDataGenerator = imageDataGenerator
-
+        self.augmentation_mode = augmentation_mode
     def random_blur(self, x):
-        radius = np.random.uniform(settings.AUGMENTATION_BLUR_RANGE[0], settings.AUGMENTATION_BLUR_RANGE[1])
+        augmentation = settings.AUGMENTATION[self.augmentation_mode]
+        radius = np.random.uniform(augmentation['BLUR_RANGE'][0], augmentation['BLUR_RANGE'][1])
         x = PIL.Image.fromarray(x)
         x = x.filter(PIL.ImageFilter.GaussianBlur(radius=radius))
         x = np.array(x)
